3420d7d15beb1e999fdb5925f9847e6aed6465b3
berkay22demirel <berkay_demirel22@hotmail.com>
2019-02-11 14:26:54 +0300

    XML view example

---
+++ beginSpringMVC/src/xmlview/ProductList.java
@@ -0,0 +1,29 @@
+package xmlview;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "productList")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ProductList {
+
+	@XmlElement
+	private List<Product> productList;
+
+	public ProductList() {
+		super();
+	}
+
+	public List<Product> getProductList() {
+		return productList;
+	}
+
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+
+}
