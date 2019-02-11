c8f657463d3fa3e7854a76472c2489370ab91c9a
berkay22demirel <berkay_demirel22@hotmail.com>
2019-02-11 14:28:15 +0300

    JSON view example

--- beginSpringMVC/src/jsonview/JSONController.java
+++ beginSpringMVC/src/jsonview/JSONController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import webstoreexample.Product;
-
 @Controller
 @RequestMapping(value = "/json")
 public class JSONController {
